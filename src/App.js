cc6e84091663a672c40b801d13e3a60d6c37c521
ninad patil <ninad.patil198@gmail.com>
2023-09-11 15:26:21 +0530

    setup basic routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl shadow-md">lets build something</div>;
+  return <Body />;
 }
 
 export default App;
